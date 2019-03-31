bd23c6206a95ad64f1b88494f3b63ac192c3a0f9
seungsukim <seungsu0407@gmail.com>
2019-03-31 21:13:28 +0900

    MVC

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
-import cookieParser from "cookie-parser"
-import bodyParser  from "body-parser"
-import { userRouter } from "./router"
+import cookieParser from "cookie-parser";
+import bodyParser  from "body-parser";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import routes from "./routes";
 
 const app = express();
-
-const handleHome = (req, res) => res.send('Hello from home');
-
-const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -17,10 +16,8 @@ app.use(bodyParser.urlencoded({ extended : true }));
 app.use(morgan("dev"));
 app.use(helmet());
 
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
-app.use("/user", userRouter);
+app.use("/", globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.home, videoRouter);
 
 export default app;
