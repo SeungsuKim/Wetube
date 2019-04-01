49793dd933bca586dc590d7ee0e04ef187df5a42
seungsukim <seungsu0407@gmail.com>
2019-04-02 01:53:10 +0900

    #3.5 Home Controller Finished

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,12 +1,19 @@
 import routes from "../routes";
+import Video from "../models/Video";
 
-export const home = (req, res) => {
-    res.render("home", { pageTitle: "Home", videos });
+export const home = async (req, res) => {
+    try {
+        const videos = await Video.find({});
+        res.render("home", { pageTitle: "Home", videos });
+    } catch (error) {
+        console.log(error);
+        res.render("home", { pageTitle: "Home", videos: [] });
+    }
 };   
 
 export const search = (req, res) => {
     const {query: { term: searchingBy }} = req;
-    res.render("search", { pageTitle: "Search", searchingBy, videos });
+    res.render("search", { pageTitle: "Search", searchingBy });
 };
 
 export const getUpload = (req, res) => {
