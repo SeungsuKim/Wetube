9b1fb3641f55950aee66e81544af283f0e58b163
seungsukim <seungsu0407@gmail.com>
2019-04-07 15:19:57 +0900

    #6.5 MongoStore and Middlewares

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,29 +1,32 @@
 import express from "express";
 import routes from "../routes"
-import { 
-    videoDetail, 
-    deleteVideo, 
+import {
+    videoDetail,
+    deleteVideo,
     getUpload,
     postUplaod,
     getEditVideo,
     postEditVideo
 } from "../controllers/videoController";
-import { uploadVideo } from "../middlewares";
+import {
+    uploadVideo,
+    onlyPrivate
+} from "../middlewares";
 
 const videoRouter = express.Router();
 
 // Uplaod
-videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, uploadVideo, postUplaod);
+videoRouter.get(routes.upload, onlyPrivate, getUpload);
+videoRouter.post(routes.upload, onlyPrivate, uploadVideo, postUplaod);
 
 // Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
 
 // Edit Video
-videoRouter.get(routes.editVideo(), getEditVideo);
-videoRouter.post(routes.editVideo(), postEditVideo);
+videoRouter.get(routes.editVideo(), onlyPrivate, getEditVideo);
+videoRouter.post(routes.editVideo(), onlyPrivate, postEditVideo);
 
 // Delete Video
-videoRouter.get(routes.deleteVideo(), deleteVideo);
+videoRouter.get(routes.deleteVideo(), onlyPrivate, deleteVideo);
 
 export default videoRouter;
