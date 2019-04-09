18ffa3606a8ff2ecbc11c603c0f9a1aa5caf20ec
seungsukim <seungsu0407@gmail.com>
2019-04-09 22:21:46 +0900

    #9.2 Recording Video

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -2,7 +2,34 @@ const recordContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordButton");
 const recordPreview = document.getElementById("jsRecordPreview");
 
+let streamObject;
+let videoRecorder;
+
+const handleVideoData = event => {
+    const { data: videoFile } = event;
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(videoFile);
+    link.download = "recorded.webm";
+    document.body.appendChild(link);
+    link.click();
+    console.log(event.data);
+}
+
 const startRecording = async () => {
+    videoRecorder = new MediaRecorder(streamObject);
+    videoRecorder.start();
+    videoRecorder.addEventListener("dataavailable", handleVideoData);
+    recordBtn.addEventListener("click", stopRecording);
+}
+
+const stopRecording = () => {
+    videoRecorder.stop();
+    recordBtn.removeEventListener("click", stopRecording);
+    recordBtn.addEventListener("click", getVideo);
+    recordBtn.innerHTML = "Start Recording";
+}
+
+const getVideo = async () => {
     try {
         const stream = await navigator.mediaDevices.getUserMedia({
             audio: true,
@@ -11,15 +38,19 @@ const startRecording = async () => {
         recordPreview.srcObject = stream;
         recordPreview.muted = true;
         recordPreview.play();
+        recordBtn.innerHTML = "Strop Recording";
+        streamObject = stream;
+        startRecording();
     } catch (error) {
         console.log(error);
         recordBtn.innerHTML = "☹️ Cannot Record";
-        recordBtn.removeEventListener("click", startRecording);
+    } finally {
+        recordBtn.removeEventListener("click", getVideo);
     }
 }
 
 function init() {
-    recordBtn.addEventListener("click", startRecording);
+    recordBtn.addEventListener("click", getVideo);
 }
 
 if (recordContainer) {
