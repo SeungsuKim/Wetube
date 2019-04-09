9876f071a4086c527c4ed85b506ed1e14512eeff
seungsukim <seungsu0407@gmail.com>
2019-04-09 22:48:35 +0900

    #10.1 API Registering a View

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ import { localsMiddleware } from "./middlewares";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import apiRouter from "./routers/apiRouter";
 import routes from "./routes";
 
 import "./passport";
@@ -41,5 +42,6 @@ app.use(localsMiddleware);
 app.use("/", globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
+app.use(routes.api, apiRouter);
 
 export default app;
